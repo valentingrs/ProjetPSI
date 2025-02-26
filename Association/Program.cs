ba3aedc6ab4571fa77c8cdcdfe9ffa5f321b6787
valentingrs <valentin.gros06@gmail.com>
2025-02-26 17:22:43 +0100

    Ajoutez des fichiers projet.

---
+++ Association/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Association
+{
+    internal class Program
+    {
+        public class Noeud
+        {
+            private int sommet;
+
+
+            public Noeud(int sommet)
+            {
+                this.sommet = sommet;
+            }
+        }
+
+        public class Lien
+        {
+            private bool lien;
+            private Noeud noeud1;
+            private Noeud noeud2;
+
+            public Lien(bool lien, Noeud noeud1, Noeud noeud2)
+            {
+                this.lien = lien;
+                this.noeud1 = noeud1;
+                this.noeud2 = noeud2;
+            }
+
+            public bool CreerLien(Noeud initial, Noeud final)
+            {
+
+            }
+        }
+
+        public class Graphe
+        {
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
